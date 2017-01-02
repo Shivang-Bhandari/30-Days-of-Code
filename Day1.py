adb94ee63ed0dcac9c561809f49832b5b6ad99c6
Shivang-Bhandari <pump382156@gmail.com>
2017-01-02 12:03:34 +0530

    Day 1 Solution Added

---
+++ Day1.py
@@ -0,0 +1,16 @@
+i=4
+d=4.0
+s='HackerRank'
+
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+a=int(raw_input())
+b=float(raw_input())
+c=str(raw_input())
+# Print the sum of both integer variables on a new line.
+print a+i
+# Print the sum of the double variables on a new line.
+print b+d
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print s+c
