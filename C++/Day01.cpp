b254d9f292f295b74d67008a6865c4db4cf92110
Shivang Bhandari <pump382156@gmail.com>
2017-03-08 14:06:40 +0530

    Day01 Solution added in C++

---
+++ C++/Day01.cpp
@@ -0,0 +1,18 @@
+// Declare second integer, double, and String variables.
+    int j;
+    double e;
+    string str2;
+    // Read and save an integer, double, and String to your variables.
+    cin >> j >> e;
+    cin.get();
+    getline(cin, str2);
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    cout << i+j << '\n';
+    cout << fixed << setprecision(1) << e+e << '\n';
+    cout << s+str2;
+    // Print the sum of both integer variables on a new line.
+
+    // Print the sum of the double variables on a new line.
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
