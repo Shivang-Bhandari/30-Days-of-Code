20ced3bc92bf8062040a904c03e5a04b5da494ee
Shivang Bhandari <pump382156@gmail.com>
2017-03-11 23:45:46 +0530

    Day5 Solution added in C++

---
+++ C++/Day05.cpp
@@ -0,0 +1,13 @@
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1;i<=10;i++){
+        int x=n*i;
+        cout << n << " x " << i <<" = "<< x << endl;
+    }
+    return 0;
+}
