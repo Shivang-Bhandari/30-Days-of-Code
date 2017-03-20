70239bb949712c2406988f42438bc7aba27e740f
Shivang Bhandari <pump382156@gmail.com>
2017-03-20 22:40:18 +0530

    Day17 Solution Added in python

---
+++ python/Day17.py
@@ -0,0 +1,11 @@
+class Calculator(object):
+    def __init__(self):
+                 self.object = object
+    def power(self, a,b):
+                 self.a = a
+                 self.b = b
+                 if self.a>=0 and self.b >=0:
+                    return (a**b)
+                 else:
+                    e = "n and p should be non-negative"
+                    return e
